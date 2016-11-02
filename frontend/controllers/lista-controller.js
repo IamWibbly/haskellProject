975023c4fcc0868c2f4aa30207ef7766673d6ebe
Davi de Carvalho <Davi de Carvalho>
2016-11-02 13:13:37 -0200

    Downloaded angular-route, jquery, list view and controller ready

--- frontend/controllers/lista-controller.js
+++ frontend/controllers/lista-controller.js
@@ -1,2 +1,15 @@
 angular.module('webapp')
-.controller('')
+  .controller('ListaController',ListaController);
+
+function ListaController(){
+  vm = this
+  vm.mensagem = "teste";
+
+  vm.listaLanchonetes = [
+    {nome:'Lanchonete1',localizacao:'Fatec Rubens Lara'},
+    {nome:'Lanchonete2',localizacao:'Fatec Rubens Lara'},
+    {nome:'Lanchonete3',localizacao:'Fatec Rubens Lara'},
+    {nome:'Lanchonete27',localizacao:'Fatec Praia Grande'},
+    {nome:'Lanchonete65',localizacao:'Fatec São José dos Campos'},
+  ]
+}
--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,13 @@
-angular.module('webapp',[]);
+angular.module('webapp',['ngRoute'])
+  .config(function($routeProvider,$locationProvider){
+  $locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/lista',{
+    templateUrl:'views/listaLanchonetes.html',
+    controller:'ListaController as lista'
+  })
+    .otherwise({
+    redirectTo:'/lista'
+  });
+});
