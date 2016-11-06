160d21a29de7b075bb6390795b61fb34e903e96b
Davi de Carvalho <Davi de Carvalho>
2016-11-06 00:36:01 -0200

    new view

---
+++ frontend/controllers/produtos-controller.js
@@ -0,0 +1,7 @@
+angular.module('webapp')
+  .controller('ProdutosController',ProdutosController);
+
+function ProdutosController(){
+  vm = this;
+  vm.message = "Produtos";
+};
--- frontend/main.js
+++ frontend/main.js
@@ -3,11 +3,19 @@ angular.module('webapp',['ngRoute'])
   $locationProvider.html5Mode(true);
 
   $routeProvider
+    .when('/',{
+    templateUrl:'views/listaLanchonetes.html',
+    controller:'ListaController as lista'
+  })
     .when('/lista',{
     templateUrl:'views/listaLanchonetes.html',
     controller:'ListaController as lista'
   })
+    .when('/produtos',{
+    templateUrl:'views/listaProdutos.html',
+    controller:'ProdutosController as produtos'
+  })
     .otherwise({
-    redirectTo:'/lista'
+    redirectTo:'/'
   });
 });
