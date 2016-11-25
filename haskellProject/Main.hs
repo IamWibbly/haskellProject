92aa6468466a257ee1ee921d3a8df0670c603837
Wibbly <alexsander.gomes1996@gmail.com>
2016-11-25 04:15:47 +0000

    dependencias do banco e conexão com heroku

--- haskellProject/Main.hs
+++ haskellProject/Main.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes,
+             TemplateHaskell #-}
 import Application () -- for YesodDispatch instance
 import Foundation
-import Yesod.Core
+import Yesod
+import Control.Monad.Logger (runStdoutLoggingT)
+import Database.Persist.Postgresql
 
-main :: IO ()
-main = warp 3000 App
+-- string de conexão
+connStr :: ConnectionString
+connStr = "dbname=d83m8f8t2v207g host=ec2-54-243-54-21.compute-1.amazonaws.com user=zjxlogygrxrujv password=tUFzCDYc4OrBL1CwIiY4ZcqN_L port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool 
+       warp 8080 (App pool)
